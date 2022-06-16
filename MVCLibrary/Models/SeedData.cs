bf0292b3ef22088e9ab4ee163debdd71b8d8564c
Κωνσταντίνος Κατσάβαρος <kkatsavaros@gmail.com>
2022-06-16 08:36:13 +0300

    initial commit

---
+++ MVCLibrary/Models/SeedData.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCLibrary.Data;
+
+namespace MVCLibrary.Models
+{
+    public class SeedData
+    {
+
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new LibraryContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<LibraryContext>>()))
+            {
+                if (context.Book.Any()) // Αν υπάρχουν βιβλία επέστρεψε.
+                {
+                    return;
+                }
+                context.Book.AddRange(
+                    new Book { Title = "Tine C# Projects", CallNumber = "AXD 2029" },
+                    new Book { Title = "Tine Android Projects", CallNumber = "bgn 5453" }
+                    );
+
+                context.SaveChanges();
+
+            }
+        }
+
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,28 @@
+ASP.NET Core 6 - CRUD Operations
+
+Database: Library
+
+
+In that programm I use ASP.NET Core 6 to Read, Delete, Update and Create to a database.
+
+It is a smoll Library that i manage Books and Authors.
+
+I use Entity Framework Core 6 for the mapping.
+
+I also use MVC Controller with views, using Entity Framework to show how .NET helps Developers to produce a project real fast.
+
+
+At the first part the produced database is empty.
+
+
+At the second part i show you how to produce the Database with start data. (Seeding).
+
+For that reason i have made a new class called "SeedData.cs" and i have added that scope in Program.cs file.
+
+
+At the third part i want to add the Author of the book. Instead of destroy all programm i add the author and i make some changes to the controllers.
+
+
+
+Katsavaros Konstantinos
+
